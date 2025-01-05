beeabe32cdb08880e40e2c79089f7eff0863f7ea
00MY00 <75865145+00MY00@users.noreply.github.com>
2025-01-05 19:00:32 +0100

    Add files via upload

--- Start_Python_HTTP_Server.sh
+++ Start_Python_HTTP_Server.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-
-# Script to reat a python HTTP server to help files transfèr to the victim
+# Script pour créer un serveur HTTP Python pour le transfert de fichiers
 # Vérifier si /tmp existe
 if [ -d "/tmp" ]; then
     echo "Le répertoire /tmp existe."
@@ -28,12 +27,29 @@ else
     echo "Le port $PORT est disponible."
 fi
 
+# Obtenir l'adresse IP locale
+LOCAL_IP=$(hostname -I | awk '{print $1}')
+
+# Optionnel : Obtenir l'adresse IP publique
+PUBLIC_IP=$(curl -s ifconfig.me)
+
 # Lancer le serveur HTTP Python
 echo "Démarrage du serveur HTTP Python sur le port $PORT..."
 python3 -m http.server $PORT &
-echo "Le serveur HTTP Python est en cours d'exécution."
 
+# Attendre un moment pour s'assurer que le serveur démarre
+sleep 2
 
-
-
-
+# Vérifier si le serveur est bien lancé
+if lsof -i:$PORT &>/dev/null; then
+    echo "Le serveur HTTP Python est en cours d'exécution."
+    echo "URL locale : http://$LOCAL_IP:$PORT"
+    if [ -n "$PUBLIC_IP" ]; then
+        echo "URL publique : http://$PUBLIC_IP:$PORT"
+    else
+        echo "Impossible de déterminer l'adresse IP publique."
+    fi
+else
+    echo "Échec du démarrage du serveur HTTP Python. Fin du script."
+    exit 1
+fi
